0337664c61ba5ef5aa93212883b16aa74567d344
Charles Magid <cmagid@rbmtechnologies.com>
2018-04-01 16:57:08 -0400

    Ex04 master first try to modify Main.elm

--- 04 first-app/Main.elm
+++ 04 first-app/Main.elm
@@ -5,46 +5,75 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
--- model
-
 
 type alias Model =
-    Int
+    { calories : Int
+    , input : Int
+    , error : Maybe String
+    }
+
+
 
 
 initModel : Model
 initModel =
-    0
-
-
-
--- update
+    -- Model 0 0 Nothing
+    { calories = 0
+    , input = 0
+    , error = Nothing
+    }
 
 
 type Msg
     = AddCalorie
     | Clear
+    | Input String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddCalorie ->
-            model + 1
+            { model
+                | calories = model.calories + model.input
+                , input = 0
+            }
+
+        -- model + 1
+        Input val ->
+            case String.toInt val of
+                Ok input ->
+                    { model
+                        | input = input
+                        , error = Nothing
+                    }
+
+                Err err ->
+                    { model
+                        | input = 0
+                        , error = Just err
+                    }
 
         Clear ->
             initModel
 
 
-
--- view
-
-
 view : Model -> Html Msg
 view model =
     div []
-        [ h3 []
-            [ text ("Total Calories: " ++ (toString model)) ]
+        [ h3 [] [ text ("Total Calories: " ++ toString model.calories) ]
+        , input
+            [ type_ "text"
+            , onInput Input
+            , value
+                (if model.input == 0 then
+                    ""
+                 else
+                    toString model.input
+                )
+            ]
+            []
+        , div [] [ text (Maybe.withDefault "" model.error) ]
         , button
             [ type_ "button"
             , onClick AddCalorie
@@ -56,6 +85,7 @@ view model =
             ]
             [ text "Clear" ]
         ]
+
 
 
 main : Program Never Model Msg
